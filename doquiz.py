661a91ae8351c690204a6c5371638b8164283956
Will Ediger <will.ediger@gmail.com>
2015-05-31 11:04:45 -0700

    initial

---
+++ doquiz.py
@@ -0,0 +1,14 @@
+import splinter
+
+browser = splinter.Browser('chrome')
+
+browser.visit('https://quizup.com/en')
+browser.fill('q', 'splinter - python acceptance testing for web applications')
+browser.find_by_name('btnG').click()
+
+if browser.is_text_present('splinter.readthedocs.org'):
+    print "Yes, the official website was found!"
+else:
+    print "No, it wasn't found... We need to improve our SEO techniques"
+
+browser.quit()
