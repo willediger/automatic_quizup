9c6abf52754490c20d08a88abd0b172662824ca5
Will Ediger <will.ediger@gmail.com>
2015-06-03 00:11:21 -0700

    parameterized the category name a little

--- doquiz.py
+++ doquiz.py
@@ -1,7 +1,11 @@
 import doquiz_selenium
 
+# category = 'peep-show'
+category = 'general-knowledge'
+
 while True:
     try:
-        doquiz_selenium.do_quizup("peep-show")
+        doquiz_selenium.do_quizup(category)
     except:
-        doquiz_selenium.do_quizup("peep-show")
+        doquiz_selenium.do_quizup(category)
+
